7f5fbfa7e4df78189e3cc48a73eb3e1102765d4d
Timur Malkoc <timurmlkc@gmail.com>
2026-01-07 23:05:14 +0300

    general ui update

--- ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe0cb37c08639c34466c03a75a8735d0e3d8a93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c6a1847e4bb496c932e7c46509d543232db987")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
