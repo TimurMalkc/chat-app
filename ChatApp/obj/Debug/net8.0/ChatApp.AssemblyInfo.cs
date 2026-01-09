a153f557b7eac47a903d108742c4ed4414d3235d
Timur Malkoc <timurmlkc@gmail.com>
2026-01-09 22:42:01 +0300

    fixed admin user ui bug

--- ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088261af44996e18fe157528683e14aba03665e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006b157c66084289205dffd4af205b45ae14e1e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
