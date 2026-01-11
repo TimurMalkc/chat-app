f5cbd980cfe55f05cd9f3ffa3984c04d1f748fde
Timur Malkoc <timurmlkc@gmail.com>
2026-01-11 23:01:06 +0300

    color change for main screen

--- ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b72ce6f69ab72df66baebf0df1d0471bf41af6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05fe37ea69c4ebe0b2e503e9d24abb067bb98ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
